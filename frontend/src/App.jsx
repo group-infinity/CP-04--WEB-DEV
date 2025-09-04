ca905901d6bbe6c2b118a4c472447d5d130067f7
Luara Soares <luafreire16@gmail.com>
2025-09-04 10:24:04 -0300

    Estilizando cards

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,15 @@
-import Navbar from "./layout/Navbar"
-import "./index.css"
+import Navbar from "./layout/Navbar";
 import Home from './pages/Home.jsx';
-import Cards from './components/Cards.jsx'
-import Footer from './components/Footer.jsx'
-
-
+import Footer from './components/Footer.jsx';
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Home></Home>
-      <Cards></Cards>
       <Footer></Footer>
     </>
   )
 }
 
-export default App
+export default App
---
+++ frontend/src/components/CarCard.jsx
@@ -0,0 +1,16 @@
+function CarCard({ nome, preco, imagem }) {
+  return (
+    <div className="bg-[rgba(0,0,0,0.85)] rounded-2xl overflow-hidden hover:shadow-2xl hover:-translate-y-1 transition-all w-full lg:w-[49%] shadow-[0px_0px_20px_-1px_rgba(0,0,0,0.75)]">
+      <div className="p-2">
+        <img src={imagem} alt={nome} className="w-full h-56 object-cover rounded-sm" />
+      </div>
+      <div className="p-5">
+        <h3 className="text-3xl font-bold text-white">{nome}</h3>
+        <p className="text-xl text-red-600 font-semibold">{preco}</p>
+        <button className="mt-4 w-full py-2 rounded-lg bg-red-600 text-white font-medium hover:bg-red-700 cursor-pointer">Ver Detalhes</button>
+      </div>
+    </div>
+  );
+}
+
+export default CarCard;
