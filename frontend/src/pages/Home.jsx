52f93e674f0049e9c7e1f5a0e2cfab7aa81f1581
Miguel <miguel.amarod37@gmail.com>
2025-09-05 16:35:00 -0300

    Correção de URL

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -61,7 +61,7 @@ const Home = () => {
   const [carros, setCarros] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:5001/carros")
+    fetch(`${import.meta.env.VITE_API_URL}/carros`)
       .then((response) => response.json())
       .then((data) => setCarros(data))
       .catch((error) => console.error("Erro ao buscar carros:", error));
